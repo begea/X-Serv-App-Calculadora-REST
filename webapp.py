1abcca90c370cf4de8acd44e8f86b3b4ba1f404c
begea <boegma@hotmail.com>
2017-03-15 01:25:47 +0100

    Archivo webapp.py

---
+++ webapp.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+
+import socket
+
+class webApp:
+
+    def parse(self, request):
+
+        return None
+
+    def process(self, parsedRequest):
+
+        return ("200 OK", "<html><body><h1>It works!</h1></body></html>")
+
+    def __init__(self, hostname, port):
+
+        # Create a TCP objet socket and bind it to a port
+        mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        mySocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        mySocket.bind((hostname, port))
+
+        # Queue a maximum of 5 TCP connection requests
+        mySocket.listen(5)
+
+        # Accept connections, read incoming data, and call
+        # parse and process methods (in a loop)
+
+        try:
+            while True:
+                print('Waiting for connections')
+                (recvSocket, address) = mySocket.accept()
+                print('HTTP request received (going to parse and process):')
+                request = recvSocket.recv(2048).decode('utf-8')
+                print(request)
+                parsedRequest = self.parse(request)
+                (returnCode, htmlAnswer) = self.process(parsedRequest)
+                print('Answering back...')
+                recvSocket.send(bytes("HTTP/1.1 " + returnCode + " \r\n\r\n"
+                                + htmlAnswer + "\r\n", 'utf-8'))
+                recvSocket.close()
+        except KeyboardInterrupt:
+            print("Closing binded socket")
+
+if __name__ == "__main__":
+    testWebApp = webApp("localhost", 1234)
